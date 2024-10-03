67f9470df2b2a7b507a4a8d75652fbb56b632f51
Estefanía <estefaniaramirezmnt@gmail.com>
2024-10-03 14:47:43 +0200

    added powers to ironman

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
-// import { Hero as SuperHero} from "./classes/Hero";
-
-// const ironman = new SuperHero('Ironman', 1, 55);
-// console.log(ironman);
-
-import * as HeroClasses from './classes/Hero';
+import { Hero } from "./classes/Hero";
 import powers from './data/powers';
 
-const Hero = 123;
-
-const ironman = new HeroClasses.Hero('Ironman', 1, 55);
+const ironman = new Hero('Ironman', 1, 55);
 console.log(ironman);
-
-console.log( powers );
+console.log( ironman.powers );
