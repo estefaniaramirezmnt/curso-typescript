d5f05c4f842e247489426127562aaaa998a6f591
Estefanía <estefaniaramirezmnt@gmail.com>
2024-10-03 14:07:20 +0200

    Added Hero.ts file and Hero class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
+import { Hero } from "./classes/Hero";
 
+// console.log('Hola Mundo?');
 
-console.log('Hola Mundo?');
-
-
-
-
-
-
-
+const ironman = new Hero('Ironman', 1, 55);
+console.log(ironman);
