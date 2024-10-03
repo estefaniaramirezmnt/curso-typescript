a9c6c1379e5e06111bc5047ba20003c7a08494c8
Estefanía <estefaniaramirezmnt@gmail.com>
2024-10-03 14:29:31 +0200

    Added file powers.ts

---
+++ src/data/powers.ts
@@ -0,0 +1,13 @@
+interface Power {
+    id: number;
+    desc: string;
+}
+
+const powers: Power[] = [
+    { id: 1, desc: 'Really Smart' },
+    { id: 2, desc: 'Super Flexible' },
+    { id: 3, desc: 'Super Hot' },
+    { id: 4, desc: 'Weather Changer' }
+];
+
+export default powers;
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@
 // console.log(ironman);
 
 import * as HeroClasses from './classes/Hero';
+import powers from './data/powers';
 
 const Hero = 123;
 
 const ironman = new HeroClasses.Hero('Ironman', 1, 55);
 console.log(ironman);
+
+console.log( powers );
